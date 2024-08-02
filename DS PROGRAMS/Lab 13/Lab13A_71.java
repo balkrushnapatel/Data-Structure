632be694645b609fd67953b1621f5bd0d6b9d576
Balkrushna Patel <141103318+balkrushnapatel@users.noreply.github.com>
2024-08-02 07:55:43 +0530

    Add files via upload

---
+++ DS PROGRAMS/Lab 13/Lab13A_71.java
@@ -0,0 +1,27 @@
+
+class Node{
+    int data;
+    Node link;
+
+    Node(int data){
+        this.data = data;
+        this.link = null;
+    }
+}
+
+Node First = null;
+Node Last = First;
+
+public class Lab13A_71 {
+
+    public void insertAtFirstCir(int data){
+        Node newNode = new Node(data);
+        if (first == null){
+            
+        }
+
+    }
+    public static void main(String[] args) {
+        
+    }
+}
